eb1de60388b2d2179143d162023731ed42abe800
Abhijeet B Kurade <64726465+abhikurade@users.noreply.github.com>
2021-04-01 18:00:28 +0530

    Create vector_sort.cpp

---
+++ STL/Vectors/vector_sort.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {	
+  
+	vector<int> v2 = {5,8,6,4,1,2,9,7,5,6,7,3,4,7};
+
+	sort(v2.begin(), v2.end());
+
+
+	vector<int> v3 = {5,8,6,4,1,2,9,7,5,6,3,4,7};
+
+	sort(v3.begin(), v3.begin() + 5);
+
+
+
+}
