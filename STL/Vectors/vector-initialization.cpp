39b20d2b4e5a8a3a39c218058bc5f3382c5f23a1
Abhijeet B Kurade <64726465+abhikurade@users.noreply.github.com>
2021-04-01 14:18:51 +0530

    Create vector-initialization.cpp

---
+++ STL/Vectors/vector-initialization.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {		
+	//initialization
+	vector<int> v ;
+
+	//creating vector of 15 elements and initialize with all 8s
+	//using assign
+	v.assign(15,8);
+
+	vector<int> v1(15,8);
+
+	for(int i :v1)
+		cout<<i<<endl;
+}
